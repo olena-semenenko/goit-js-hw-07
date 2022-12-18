af8ba5c9b023ce922f2d112c700ccbdf52b2bd7d
olena-semenenko <elen.kinakh@gmail.com>
2022-12-18 22:41:59 +0100

    Added solutions

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener('click', showImage);
+
+function createMarkup(array) {
+  const markup = array.reduce((acc, item) => {
+    const { preview, original, description } = item;
+    acc += `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    return acc;
+  }, '');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+createMarkup(galleryItems);
+
+function showImage(event) {
+  event.preventDefault();
+  if (event.target.classList.contains('gallery__image')) {
+    const url = event.target.dataset.source;
+    console.log(url);
+    const instance = basicLightbox.create(`<img src="${url}" width="800" height="600">`);
+    instance.show();
+
+    gallery.addEventListener('keydown', closeImage);
+    function closeImage(event) {
+      if (event.code === 'Escape') {
+        instance.close();
+      }
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener('click', showImage);
+
+function createMarkup(array) {
+  const markup = array.reduce((acc, item) => {
+    const { preview, original, description } = item;
+    acc += `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    return acc;
+  }, '');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+createMarkup(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
+
+function showImage(event) {
+  event.preventDefault();
+  if (event.target.classList.contains('gallery__image')) {
+    const title = event.target.alt;
+    lightbox.defaultOptions.captionType = 'text';
+    lightbox.defaultOptions.captionClass = `${title}`;
+    lightbox.defaultOptions.captionDelay = 250;
+  }
+}
